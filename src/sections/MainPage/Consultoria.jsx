5e51158b22ccef661cc8adcb1c211fdb94754613
Henrique <henriquepinheiroxavier@gmail.com>
2023-11-14 21:07:40 -0300

    feat: consutory file created

---
+++ src/sections/MainPage/Consultoria.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ContainerConsultoria = styled.div`
+
+`
+
+const Consultoria = () => {
+  return (
+    <ContainerConsultoria>Consultoria</ContainerConsultoria>
+  )
+}
+
+export default Consultoria
